b306284a4cb60d420daaea21c229aee4108dc29e
Nikolay Petrov <nikip1303@gmail.com>
2023-11-18 19:08:12 +0200

    using Character hero ref

--- src/Game.java
+++ src/Game.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public abstract class Game {
     Queue<Monster> monster;
     private Character hero;
-    
+
 
     public void createHero(String name, String race) throws WrongRaceException{
         if(race.equals("Paladin")){
-            Paladin paladin = new Paladin(name, 150, 25, race);
+            this.hero = new Paladin(name, 150, 25, race);
         } else if (race.equals("Witcher")) {
-            Wicher wicher = new Wicher(name, 120, 30, race);
+            this.hero = new Wicher(name, 120, 30, race);
         } else if (race.equals("Varvar")) {
-            Varvar varvar = new Varvar(name, 180, 35, race);
+            this.hero = new Varvar(name, 180, 35, race);
         } else if (race.equals("Nekromant")) {
-            Nekromant nekromant = new Nekromant(name, 130, 25, race);
+            this.hero = new Nekromant(name, 130, 25, race);
         } else if (race.equals("Amazon")) {
-            Amazon amazon = new Amazon(name, 100, 20, race);
+            this.hero = new Amazon(name, 100, 20, race);
         }
         else if(race.isEmpty()){
             throw new WrongRaceException();
@@ -25,6 +25,7 @@ public abstract class Game {
         else{
             throw new  WrongRaceException();
         }
+        System.out.println(this.hero.getPower());
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main extends Game{
     public static void main(String[] args) {
-            Paladin myPaladin = new Paladin("Bat Gergi", 100, 25, "Paladin");
-            SkeletonKing SkeletonKing = new SkeletonKing("King", 80, 30, "Skeleton King");
 
-
-            while (myPaladin.getHealth() != 0 && SkeletonKing.getHealth() != 0){
-                System.out.println(myPaladin.defend(SkeletonKing.getPower()));
-                //System.out.println(SkeletonKing.defend(myPaladin.getPower()));
-            }
-
-
-            if(myPaladin.getHealth() == 0 && SkeletonKing.getHealth() == 0){
-                System.out.println("No winner both are dead!");
-            }
-            if(myPaladin.getHealth() == 0){
-                System.out.println(SkeletonKing.getName() + " is a winner!");
-            }
-            if(SkeletonKing.getHealth() == 0){
-                System.out.println(myPaladin.getName() + " is a winner!");
-            }
+           
         }
     }
