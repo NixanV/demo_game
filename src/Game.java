275e697926c6d71ea827d97324f46dd09fa88b12
Nikolay Petrov <nikip1303@gmail.com>
2023-11-18 19:34:00 +0200

    creating addMonster method

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Game {
-    Queue<Monster> monster;
+    Queue<Monster> monster = new LinkedList<>();
     private Character hero;
 
 
@@ -27,5 +28,24 @@ public class Game {
         }
         System.out.println(this.hero.getPower());
     }
+
+    public void addMonster(String monster) throws WrongRaceException{
+        if(monster.equals("FallenShaman")){
+            this.monster.add(new FallenShaman("FallenShaman", 40, 26, "FallenShaman"));
+        }
+        else if(monster.equals("SkeletonKing")){
+            this.monster.add(new SkeletonKing("SkeletonKing", 50, 40, "SkeletonKing"));
+        }
+        else if(monster.equals("Butcher")){
+            this.monster.add(new Butcher("Butcher", 60, 32, "Butcher"));
+        }
+        else if(monster.isEmpty()){
+            throw new WrongRaceException();
+        }
+        else{
+            throw new WrongRaceException();
+        }
+    }
+
 
 }
