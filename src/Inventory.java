926535a49bb182a4488e958f5a12af6bc62c7faf
Nikolay Petrov <nikip1303@gmail.com>
2023-11-23 10:29:06 +0200

    creation of class item and class Inventory

---
+++ src/Inventory.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<String> items = new ArrayList<>();
+
+    public void addItem(String item){
+
+    }
+
+}
