411275a260c552244afae7246a661bb8fabbe9c1
Nikolay Petrov <nikip1303@gmail.com>
2023-11-27 13:21:22 +0200

    constructor get and set methods created in Item class

--- src/Inventory.java
+++ src/Inventory.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class Inventory {
     private List<String> items = new ArrayList<>();
 
-    public void addItem(String item){
-        items.add(item);
+    public void addItem(Item item){
+        items.add(item.getName());
     }
 
     public void removeItem(String i){
--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,14 @@
 public class Item {
     private String name;
-    
+
+    public Item(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        return name;
+    }
 }
